0e52e9e8cf47787f2bb54a8aaa162dc922357f1b
Marco Busemann <marco.busemann@live.com>
2017-12-14 11:50:46 +0100

    Keep scope of mongodb connection small

--- src/server/modules/store/ModuleStore.ts
+++ src/server/modules/store/ModuleStore.ts
@@ -5,50 +5,57 @@ import { MongoClient, Db } from 'mongodb';
 import * as _ from 'lodash';
 
 export class ModuleStore {
-    private db: Db = null;
-
     constructor(private pubsub: IMessageHub) {
-
-        MongoClient.connect(process.env.MONGODB_URL, (error, client) => {
-            if (error)
-                throw error;
-            this.db = client.db('eventstore');
-            console.log('Module store running...');
-        }); 
-
         pubsub.subscribe(TOPIC_TEST_COMPLETED, (message: string, data: any) => {
             this.storeTest(data as ITestResult);
             console.log('Storing test', data);
         });
+
+        console.log('Module store running...');
     }
 
     private storeTest(testResult: ITestResult) {
-        const collection = this.db.collection('tests');
+        this.withMongoDb((db, closeConnection) => {
+            const collection = db.collection('tests');
+            
+            collection.insertOne(testResult, (error, result) => {
+                if (error)
+                    return console.log(error);
+    
+                collection
+                    .find({ testName: testResult.testName })
+                    .sort({ timestamp: -1 })
+                    .limit(2)
+                    .toArray((error, documents) => {
 
-        collection.insertOne(testResult, (error, result) => {
-            if (error)
-                return console.log(error);
+                        closeConnection();
 
-            collection
-                .find({ testName: testResult.testName })
-                .sort({ timestamp: -1 })
-                .limit(2)
-                .toArray((error, documents) => {
-                    if (error)
-                        return console.log(error);
-
-                    if (documents.length <= 1)
-                        return console.log('Did not found two tests for', testResult);
-
-                    const comp = new TestResultComperator(
-                        documents[0] as ITestResult, 
-                        documents[1] as ITestResult);
-
-                    console.log('Comparing two tests: ', documents[0], documents[1]);
-
-                    if (!comp.equal())
-                        this.pubsub.publish(TOPIC_TESTRESULT_CHANGED, testResult);
-                });
+                        if (error)
+                            return console.log(error);
+    
+                        if (documents.length <= 1)
+                            return console.log('Did not found two tests for', testResult);
+    
+                        const comp = new TestResultComperator(
+                            documents[0] as ITestResult, 
+                            documents[1] as ITestResult);
+    
+                        console.log('Comparing two tests: ', documents[0], documents[1]);
+    
+                        if (!comp.equal())
+                            this.pubsub.publish(TOPIC_TESTRESULT_CHANGED, testResult);
+                    });
+            });
         });
+    }
+
+    private withMongoDb(callback: (db: Db, closeConnection: () => void) => void) {
+        MongoClient.connect(process.env.MONGODB_URL, (error, client) => {
+            if (error)
+                throw error;
+            callback(client.db('eventstore'), () => {
+                client.close();
+            });
+        }); 
     }
 }
