4932ba226218d3791a835fd29b0be4cd34db668c
Marco Busemann <marco.busemann@live.com>
2018-11-24 16:05:02 +0100

    Add socket.io for live client communication

--- fuse.js
+++ fuse.js
@@ -1,40 +1,36 @@
-const {FuseBox, WebIndexPlugin, CSSPlugin} = require("fuse-box");
+const { FuseBox, WebIndexPlugin, CSSPlugin } = require("fuse-box");
 
 const fuse = FuseBox.init({
-   homeDir: "src",
-   output: "dist/$name.js",
-   sourceMaps: true,
-   plugins: [
-      WebIndexPlugin({
-         template: "src/client/index.html", 
-         target: "public/index.html", 
-         bundles: [
-            "public/bundle"
-         ]
-      }),
-      CSSPlugin(),
-   ]
+    homeDir: "src",
+    output: "dist/$name.js",
+    sourceMaps: true,
+    plugins: [
+        CSSPlugin(),
+        WebIndexPlugin({
+            template: "src/client/index.html",
+            target: "public/index.html",
+            bundles: ["public/bundle"]
+        })
+    ]
 });
 
 fuse.dev({
-   port: 4445,
-   httpServer: false,
+    port: 4445,
+    httpServer: false
 });
 
-fuse
-   .bundle("server")
-   .watch("server/**")
-   .target("server@esnext")
-   .instructions(" > [server/index.ts]")
-   .completed(proc => {
-      proc.start();
-   });
+fuse.bundle("server")
+    .watch("server/**")
+    .target("server@esnext")
+    .instructions(" > [server/index.ts]")
+    .completed(proc => {
+        proc.start();
+    });
 
-fuse
-   .bundle("public/bundle")
-   .watch("client/**")
-   .target("browser")
-   .hmr()
-   .instructions(" > client/index.tsx");
+fuse.bundle("public/bundle")
+    .watch("client/**")
+    .target("browser")
+    .hmr()
+    .instructions(" > client/index.tsx");
 
 fuse.run();
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as morgan from "morgan";
+import { Server as HttpServer } from "http";
+import * as socketIo from "socket.io";
 
 import { AppRouter } from "./router";
 import { config } from "./config";
@@ -11,12 +13,15 @@ import { StaticTestsImport } from "./components/StaticTestsImport";
 import { DebugConfiguration } from "./components/DebugConfiguration";
 import { PersistedTestResults } from "./components/PersistedTestResults";
 import { ChangeDetection } from "./components/ChangeDetection";
-import { FileSystemConfiguration } from './components/FileSystemConfiguration';
-import { MattermostNotifications } from './components/MattermostNotifications';
+import { FileSystemConfiguration } from "./components/FileSystemConfiguration";
+import { MattermostNotifications } from "./components/MattermostNotifications";
 
 let configuration: IConfiguration = null;
 if (process.env.NODE_ENV === "production") {
-    configuration = new FileSystemConfiguration(config.configFile, config.mongodbUrl);
+    configuration = new FileSystemConfiguration(
+        config.configFile,
+        config.mongodbUrl
+    );
 } else {
     configuration = new DebugConfiguration();
 }
@@ -24,11 +29,24 @@ if (process.env.NODE_ENV === "production") {
 const messageHub = new PubSubMessageHub();
 const scheduledTasks = new ScheduledTasks(configuration, messageHub);
 const staticTests = new StaticTestsImport(configuration, messageHub);
-const persistedTestResults = new PersistedTestResults(configuration, messageHub);
+const persistedTestResults = new PersistedTestResults(
+    configuration,
+    messageHub
+);
 const changeDetection = new ChangeDetection(messageHub);
-const mattermostNotifications = new MattermostNotifications(configuration, messageHub);
+const mattermostNotifications = new MattermostNotifications(
+    configuration,
+    messageHub
+);
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = socketIo(httpServer);
+
+io.on("connection", function(socket) {
+    console.log("a user connected");
+});
+
 app.use(morgan("tiny"));
 
 setTimeout(async () => {
@@ -39,7 +57,7 @@ setTimeout(async () => {
     staticTests.importTests();
 
     app.use(AppRouter(scheduledTasks));
-    app.listen(config.port, () => {
+    httpServer.listen(config.port, () => {
         console.log(`Server listening on port ${config.port}!`);
     }).on("error", console.log);
 }, 0);
