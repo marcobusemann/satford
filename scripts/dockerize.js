634591906d3836c6c69d43bcaca9da00f4a9b484
Marco Busemann <marco.busemann@live.com>
2018-12-11 20:45:36 +0100

    Refactor release version scripts

--- scripts/dockerize.js
+++ scripts/dockerize.js
@@ -4,7 +4,6 @@ const path = require('path');
 const packageConfig = require(path.join(__dirname, '..', 'package.json'));
 
 const commands = [
-    'npm run bundle',
     'docker build -t blural/satford:latest .',
     'docker tag blural/satford:latest blural/satford:' + packageConfig.version,
     'docker push blural/satford:latest',
